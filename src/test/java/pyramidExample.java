3ba8bc46b975ddf360751787c9ad78ece5776fbe
giora.tovim <tovim45@gmail.com>
2022-05-30 17:10:58 +0300

    update name

--- src/test/java/pyramidExample.java
+++ src/test/java/pyramidExample.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class findGoogle {
+public class pyramidExample {
 
   public static void main(String[] args) {
     System.setProperty("webdriver.chrome.driver",
