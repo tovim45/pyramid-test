d7f5973f64f3a2140123a58b307a0cfdc3c24326
giora.tovim <tovim45@gmail.com>
2022-05-30 15:20:54 +0300

    update driver path

---
+++ src/test/java/findGoogle.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class findGoogle {
+
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver",
+        "src/main/resources/drivers/chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    //URL launch
+    driver.get("https://www.pyramidanalytics.com/");
+  }
+}
